51ecaf51685127aeffde88907d39ed89170420e8
Gordon <mr.gordon.comstock@protonmail.com>
2021-10-07 18:58:46 +0100

    feat: pools page UI

--- pages/app/pools.js
+++ pages/app/pools.js
@@ -1,8 +1,12 @@
+import Contract from "../../components/Contract"
+
 function pools() {
 	return (
-		<div>
-			
-		</div>
+		<div className="flex flex-col gap-10 sm:gap-20 w-full">
+			<Contract type="pool" />
+			<Contract type="pool" />
+			<Contract type="pool" />
+    </div>
 	)
 }
 
