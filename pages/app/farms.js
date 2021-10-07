36bfdf4b8cf31e05f7a0a31ee30572fe0b974ec0
Gordon <mr.gordon.comstock@protonmail.com>
2021-10-07 18:58:36 +0100

    feat: farms page UI

--- pages/app/farms.js
+++ pages/app/farms.js
@@ -1,9 +1,13 @@
+import Contract from "../../components/Contract";
+
 function farms() {
-	return (
-		<div className="flex flex-col sm:flex-row gap-10 sm:gap-20 w-full">
-			
-		</div>
-	)
+  return (
+    <div className="flex flex-col gap-10 sm:gap-20 w-full">
+			<Contract type="farm" />
+			<Contract type="farm" />
+			<Contract type="farm" />
+    </div>
+  );
 }
 
-export default farms
+export default farms;
